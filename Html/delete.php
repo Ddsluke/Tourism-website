86f4c0953439e0248a3d494d852040e6d1540d5c
QinYaxue <35437659+QinYaxue@users.noreply.github.com>
2018-11-23 19:47:07 +0800

    Add files via upload

---
+++ Html/delete.php
@@ -0,0 +1,16 @@
+<?php
+//delete.php
+if(isset($_POST["id"]))
+{
+ $connect = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+ $query = "
+ DELETE from event WHERE id=:id
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':id' => $_POST['id']
+  )
+ );
+}
+?>
---
+++ Html/load.php
@@ -0,0 +1,18 @@
+<?php
+$connect = new PDO('mysql:host=##;dbname=##', 'root', '');
+$data = array();
+$query = "SELECT * FROM event ORDER BY id";
+$statement = $connect->prepare($query);
+$statement->execute();
+$result = $statement->fetchAll();
+foreach($result as $row)
+{
+ $data[] = array(
+  'id'   => $row["id"],
+  'title'   => $row["title"],
+  'start'   => $row["start_event"],
+  'end'   => $row["end_event"]
+ );
+}
+echo json_encode($data);
+?>
