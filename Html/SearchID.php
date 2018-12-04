265d61a94e1f4f3090d28936514b799a1a238e19
YU Jing <yuki5834444@gmail.com>
2018-12-04 21:08:39 +0800

    update

---
+++ Html/SearchID.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Attractions#</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link type="text/css" rel="stylesheet" href="css/style.css">
+<link type="text/css" rel="stylesheet" href="css/itemDisplay.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<div class="wrapper">
+<?php
+require('topnav.php');
+?>
+</div>
+<div class="container" >
+<table>
+<?php
+    $servername = "mysql.comp.polyu.edu.hk";
+    $username = "16098537d";//need to change to xiajialu's
+    $password = "iqdobdiy";
+    $dbname="16098537d";
+    // CONNECT
+    $conn =mysql_connect($servername, $username, $password);
+    // Check connection
+    if (!$conn) {
+        die("CAN'T CONNECT : " . mysql_error());
+    }
+    mysql_select_db($dbname,$conn);
+    $type="1";
+    $name="1";
+    if(isset($_POST['types']))
+    {
+        $type=$_POST['types'];
+    }
+    if(isset($_POST['name']))
+    $name=$_POST['name'];
+    if($type!="1"&&$name!="1")
+    {
+        if($type=="Attraction"){
+            $sql1="SELECT * FROM Attraction WHERE AName LIKE '%$name%'";
+            $result1 = mysql_query($sql1,$conn);
+            if(!$result1)
+            {
+                $type="1";
+                $name="1";
+                echo "No result";
+                mysql_close($conn);
+            }
+            if(mysql_affected_rows($conn)==0){
+                $type="1";
+                $name="1";
+                echo "No record";
+                mysql_close($conn);
+                exit;}
+			?>
+        <tr>
+        <th>ID</th>
+        <th>name</th>
+        <th>price</th>
+        <th>area</th>
+        <th>typeid</th>
+        <th>type</th>
+        </tr>
+			<?php
+            while($row = mysql_fetch_array($result1)){
+                ?>
+        <tr>
+        <td><?php echo $row[0]?></td>
+        <td><?php echo $row[1]?></td>
+        <td><?php echo $row[2]?></td>
+        <td><?php echo $row[4]?></td>
+             <?php
+                $AID=$row[0];
+                $sql2="SELECT ATID from Attractions_Type where AID='$AID'";
+                $result2 =mysql_query($sql2,$conn);
+                $row = mysql_fetch_array($result2);
+                $ATID=$row[0];
+                $sql3="SELECT * from AttractionsType where ATID='$ATID'";
+                $result3 =mysql_query($sql3,$conn);
+                while($row = mysql_fetch_array($result3)){
+                    ?>
+            <td><?php echo $row[0]?></td>
+            <td><?php echo $row[1]?></td>
+        </tr>
+<?php
+            }
+        }
+    }
+        else if($type=="Hotel"){
+            $sql1="SELECT * FROM Hotel WHERE HName LIKE '%$name%'";
+            $result1 = mysql_query($sql1,$conn);
+            if(!$result1)
+            {
+                $type="1";
+                $name="1";
+                echo "No result";
+                mysql_close($conn);
+            }
+            if(mysql_affected_rows($conn)==0){
+                $type="1";
+                $name="1";
+                echo "No record";
+                mysql_close($conn);
+                exit;
+            }
+            ?>
+<tr>
+<th>ID</th>
+<th>name</th>
+<th>area</th>
+<th>level</th>
+<th>roomtypeID</th>
+<th>roomType</th>
+<th>price</th>
+</tr>
+         <?php
+            while($row = mysql_fetch_array($result1)){ ?>
+            <tr>
+                <td><?php echo $row[0]?> </td>
+                <td><?php echo $row[1]?> </td>
+                <td><?php echo $row[2]?> </td>
+                <td><?php echo $row[3]?> </td>
+<?php
+                $HID=$row[0];
+                $sql2="SELECT * from RoomInfor where Hotel_HID='$HID'";
+                $result2 =mysql_query($sql2,$conn);
+                $count=0;
+                while($row = mysql_fetch_array($result2)){
+                    $count++;
+                    if($count>0){   ?>
+                    <tr>
+                    <td><?php echo " " ?> </td>
+                    <td><?php echo " " ?> </td>
+                    <td><?php echo " " ?> </td>
+                    <td><?php echo " " ?> </td>
+                    <?php }
+                        ?>
+
+                <td><?php echo $row[0]?> </td>
+                <td><?php echo $row[1]?> </td>
+                <td><?php echo $row[2]?> </td>
+            </tr>
+<?php
+                }
+            }
+        }
+        else if($type=="Restaurant"){
+            $sql1="SELECT * FROM Restaurant WHERE RName LIKE '%$name%'";
+            $result1 = mysql_query($sql1,$conn);
+            if(!$result1)
+            {
+                $type="1";
+                $name="1";
+                echo "No result";
+                mysql_close($conn);
+            }
+            if(mysql_affected_rows($conn)==0){
+                $type="1";
+                $name="1";
+                echo "No record";
+                mysql_close($conn);
+                exit;
+            }
+            ?>
+    <tr>
+        <th>ID</th>
+        <th>name</th>
+        <th>price</th>
+        <th>area</th>
+        <th>foodtypeid</th>
+        <th>foodType</th>
+        </tr>
+<?php
+            while($row = mysql_fetch_array($result1)){
+?>
+<tr>
+    <td><?php echo $row[0]?></td>
+    <td><?php echo $row[1]?></td>
+    <td><?php echo $row[2]?></td>
+    <td><?php echo $row[3]?></td>
+<?php
+                $RID=$row[0];
+                $sql2="SELECT FID from Restaurant_Foodtype where RID='$RID'";
+                $result2 =mysql_query($sql2,$conn);
+                $row = mysql_fetch_array($result2);
+                $FID=$row[0];
+                $sql3="SELECT * from FoodType where FID='$FID'";
+                $result3 =mysql_query($sql3,$conn);
+                $count=0;
+                while($row = mysql_fetch_array($result3)){
+                    $count++;
+                    if($count>0){   ?>
+                <tr>
+                <td><?php echo " " ?> </td>
+                <td><?php echo " " ?> </td>
+                <td><?php echo " " ?> </td>
+                <td><?php echo " " ?> </td>
+            <?php }
+                        ?>
+    <td><?php echo $row[0]?></td>
+    <td><?php echo $row[1]?></td>
+    </tr>
+<?php
+                }
+            }
+        }
+    }
+
+    mysql_close($conn);
+     ?>
+</table>
+</div>
+</body>
+</html>
---
+++ Html/SearchID_form.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--
+Author: Code Apes.
+-->
+<html>  
+<head>
+	<title>Advanced Search</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+	<link type="text/css" rel="stylesheet" href="css/formStyle.css">
+	<script>
+	<!-- Space for javascript uses-->
+	function checkType(){
+		if (document.getElementById("type").value == "default"){
+			document.getElementById("type_error").style.display='block';
+			document.getElementById("confirm").disabled = true;
+		}
+		else{
+			document.getElementById("type_error").style.display='none';
+			document.getElementById("confirm").disabled = false;
+		}
+	}
+	function checkPrice() {
+		var n = document.getElementById("price").value;
+		if (!isNaN(parseFloat(n)) && isFinite(n)) {
+			document.getElementById("price_error").style.display='block';
+			document.getElementById("confirm").disabled = true;
+		}
+		else {
+			document.getElementById("price_error").style.display='none';
+			document.getElementById("confirm").disabled = false;
+		}
+}
+	
+</script>
+</head>
+
+<body>
+<div class="wrapper">
+	<?php
+    require('topnav.php');
+	?>
+</div>
+
+<div class="form_container">
+  <h1>Advanced Search</h1>
+  <form action="SearchID.php" method="post">
+	<ul class="form_input">
+	
+		<li>
+		<label for="">Type</label>
+		<select id="type" name="types" onclick="checkType()">
+			<option value="default">--Please choose--</option>
+			<option value="Attraction" >Attractions</option>
+			<option value="Hotel">Hotel</option>
+			<option value="Restaurant">Restaurants</option>
+		</select>
+		<div id="type_error" class="error">Please choose a type you want to search.</div>
+		</li>
+	
+		<li>
+        <label for="">Name</label>
+		<input type="text" id="name" name="name" placeholder="keyword..." title="No longer than 50 characters" maxlength="50">
+		</li>
+
+	    <div id="submit">
+			<input type="submit" value="Search"/>
+        </div>
+	
+</ul>
+</form>
+</div>
+
+</body>
+
+</html>
