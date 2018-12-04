f67239e1bdbe7f6f922378c375f97ece8dead51e
QinYaxue <35437659+QinYaxue@users.noreply.github.com>
2018-12-04 18:17:10 +0800

    Update PersonInfo.php

--- Html/PersonInfo.php
+++ Html/PersonInfo.php
@@ -178,7 +178,7 @@ require('topnav.php');
     if ($conn->connect_error) {
         die("CAN'T CONNECT : " . $conn->connect_error);
     } 
-    $sql=" select * from Message WHERE TouristsID = $key";
+    $sql=" select * from Message WHERE TouristsID = $key ORDER BY create_time";
 ?>
 
 <div class="content" style="text-align:center;">
