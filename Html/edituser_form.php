05822974cdaa765a7261165dfd60172c8992880f
YU Jing <yuki5834444@gmail.com>
2018-12-05 12:49:48 +0800

    admin error handle

--- Html/edituser_form.php
+++ Html/edituser_form.php
@@ -55,22 +55,22 @@ function checkEmail() {
 
 <li>
 <label for="">ID</label>
-<input type="text" id="id" name="TouristsID" placeholder="Tourists's ID.." title="No longer than 8 characters" maxlength="8">
+<input type="text" id="id" name="TouristsID" placeholder="Tourists's ID.." title="No longer than 8 characters" maxlength="8" required="required">
 </li>
 
 <li>
 <label for="">Name</label>
-<input type="text" id="name" name="Username" placeholder="Username.." title="No longer than 50 characters" maxlength="50">
+<input type="text" id="name" name="Username" placeholder="Username.." title="No longer than 50 characters" maxlength="50" required="required">
 </li>
 
 <li>
 <label for="">Age</label>
-<input type="number" id="age" name="Age" placeholder="User's age.." title="must be a valid number" maxlength="50">
+<input type="number" id="age" name="Age" placeholder="User's age.." title="must be a valid number" maxlength="50" required="required">
 </li>
 
 <li>
 <label for="">E-mail</label>
-<input type="email" id="email" name="Email" placeholder="User's email.." title="must be a valid email" maxlength="50">
+<input type="email" id="email" name="Email" placeholder="User's email.." title="must be a valid email" maxlength="50" required="required">
 <div id="email_error" style="display:none;
 font-family:Verdana, Geneva, sans-serif;    width: 100%;
 padding: 12px 20px;    margin: 8px 0;
@@ -83,15 +83,12 @@ You email is not valid.</div>
 <li>
 <label for="">Gender</label>
 <select id="gender" name="Gender" onclick="checkgender()">
-<option value="default">--Please choose--</option>
 <option value="F">Female</option>
 <option value="M">Male</option>
 <option value="others">others</option>
 </select>
 <div id="gender_error" class="error">Please choose a gender.</div>
 </li>
-
-
 
 <div id="submit">
 <input type="submit" value="Submit"/>
